50545201a8004e8397ef9982288555edb2e4bf04
malithiperera <malithiperera1998@gmail.com>
2022-07-03 22:54:49 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contact from './components/Contact';
 
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-import Projects from './components/Projects';
+
 import Skills from './components/Skills';
 
 function App() {
